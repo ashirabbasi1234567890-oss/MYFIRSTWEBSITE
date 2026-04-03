2d5f23879200299465adf3f3d6702217b352aa39
ashirabbasi1234567890 <ashirabbasi1234567890@gmail.com>
2026-04-03 17:40:33 +0500

    validation in java script.html

---
+++ app.js
@@ -0,0 +1,13 @@
+function clearScreen (){
+    document.getElementById("result").value="";
+}
+function setScreenValue (value) {
+const r=document.getElementById("result");
+if (r.value==="enter an expression"|| r. value === "invalid expression") r.value="";
+r.value+=value;
+}
+function calculateresult(){
+const resultelement=documentgetElementById
+
+
+}
